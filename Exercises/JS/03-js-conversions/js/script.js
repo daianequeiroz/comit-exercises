fd98659236d8ce548773e28c1803c7737ce85f69
Daiane Queiroz <daianequeiroz@gmail.com>
2020-09-23 15:14:11 -0500

    Lesson 20

---
+++ Exercises/JS/03-js-conversions/js/script.js
@@ -0,0 +1,17 @@
+let age = 36;
+let firstName = "Daiane";
+let courseCompleted = false;
+let finalScore = null;
+let winningNumbers;
+
+console.log("Age: ", age);
+console.log("First name: ", firstName);
+console.log("Completed this course: ", courseCompleted);
+console.log("Final score: ", finalScore);
+console.log("Winning numbers: ", winningNumbers);
+
+console.log("Age: ", String(age) );
+console.log("FirstName: ", Boolean(firstName) );
+console.log("Completed this course: ", String(courseCompleted) );
+console.log("Final score: ", Number(finalScore) );
+console.log("Winning numbers: ", Boolean(winningNumbers) );
