2602d86f5fdec9bcd50c1933f568c514d7b01b1e
Daiane Queiroz <daianequeiroz@gmail.com>
2020-09-24 16:09:16 -0500

    Lesson 21

---
+++ Exercises/JS/04-js-comparison/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+alert("Enter two numbers to see if the first is greater than the second");
+
+
+let firstNumber = Number(prompt("Enter the first number:"));
+let secondNumber = Number(prompt("Enter the second number:"));
+confirm(firstNumber > secondNumber);
+
+
