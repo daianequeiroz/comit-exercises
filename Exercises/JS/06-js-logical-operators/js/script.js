aaa6cfa8371bf9bbba65e84ca04f2f07328070a7
Daiane Queiroz <daianequeiroz@gmail.com>
2020-09-25 18:55:49 -0500

    Lesson 23

---
+++ Exercises/JS/06-js-logical-operators/js/script.js
@@ -0,0 +1,11 @@
+let hour = Number(prompt ("Enter the current hour (out of 0-23):"));
+
+
+
+if (hour < 0 || hour > 23){
+    alert("Input is incorrect!");
+}else if (hour < 8 || hour >= 18){
+    alert("Outside of office hours!");
+}else if (hour >= 8 && hour < 18){
+    alert("We are open!")
+}
